2c0e12a7a26d0ae81f46b7a6f76cd33eadc2fbb8
Ivo Alves <ivo@bcl.pt>
2021-02-15 11:11:06 +0000

    Spawn in a grid

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -1,3 +1,4 @@
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Physics;
@@ -8,8 +9,8 @@ using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
 {
-    const float V = .05f;
     public static int G = 45000;
+    static int _spawnCount;
 
     [SerializeField] GameObject _prefab;
     [SerializeField] int _spawnQuanity = 5;
@@ -17,10 +18,11 @@ public class Spawner : MonoBehaviour
     [SerializeField] Text _text;
     [SerializeField] Mesh _mesh;
     [SerializeField] UnityEngine.Material _material;
+    [SerializeField] float timePerSpawn = 1;
 
     BlobAssetStore _blob;
-    int _spawnCount;
     Entity _entityPrefab;
+    float _timeSinceSpawn;
 
     private void Start()
     {
@@ -36,10 +38,10 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
-            G *= 5;
+            G += 5000;
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
-            G /= 5;
+            G -= 5000;
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
             Time.timeScale = 1;
@@ -55,29 +57,43 @@ public class Spawner : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space))
         {
-            for (int i = 0; i < _spawnQuanity; i++)
-            {
-                Spawn();
-            }
+            Spawn();
 
             _text.text = "object count: " + _spawnCount;
         }
+
+        _timeSinceSpawn += Time.deltaTime;
     }
 
     void Spawn()
     {
+        if (_timeSinceSpawn <= timePerSpawn)
+            return;
+
         EntityManager manager = World.DefaultGameObjectInjectionWorld.EntityManager;
-        Entity entity = manager.Instantiate(_entityPrefab);
 
-        Vector3 spawnPosition = transform.position + (UnityEngine.Random.insideUnitSphere * _spawnQuanity);
+        Vector3 initialVelocity = transform.forward * _initialSpeed;
 
-        var velocityVariation = new Vector3(UnityEngine.Random.Range(-V, V), UnityEngine.Random.Range(-V, V), UnityEngine.Random.Range(-V, V));
-        Vector3 initialVelocity = (transform.forward + velocityVariation) * _initialSpeed;
+        int total = _spawnQuanity * _spawnQuanity;
+        int index = 0;
+        NativeArray<Entity> cubes = new NativeArray<Entity>(total, Allocator.TempJob);
+        manager.Instantiate(_entityPrefab, cubes);
 
-        manager.SetComponentData(entity, new Translation { Value = spawnPosition });
-        manager.SetComponentData(entity, new PhysicsVelocity { Linear = initialVelocity });
+        for (int i = 0; i < _spawnQuanity; i++)
+        {
+            for (int j = 0; j < _spawnQuanity; j++)
+            {
+                Vector3 spawnPosition = transform.position + new Vector3(0, i * 2, -j * 2);
 
-        _spawnCount++;
+                manager.SetComponentData(cubes[index], new Translation { Value = spawnPosition });
+                manager.SetComponentData(cubes[index], new PhysicsVelocity { Linear = initialVelocity });
+                index++;
+            }
+        }
+
+        _spawnCount += total;
+        _timeSinceSpawn = 0;
+        cubes.Dispose();
     }
 
     Entity CreateEntityFromPrefab()
@@ -89,66 +105,66 @@ public class Spawner : MonoBehaviour
         return entity;
     }
 
-    Entity CreateCubeEntity()
-    {
-        EntityManager manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+    //Entity CreateCubeEntity()
+    //{
+    //    EntityManager manager = World.DefaultGameObjectInjectionWorld.EntityManager;
 
-        EntityArchetype archetype = manager.CreateArchetype
-        (
-            typeof(Translation),
-            typeof(Rotation),
-            typeof(LocalToWorld),
-            typeof(RenderMesh),
-            typeof(RenderBounds),
-            typeof(PhysicsVelocity),
-            typeof(PhysicsDamping),
-            typeof(PhysicsGravityFactor),
-            typeof(PhysicsCollider),
-            typeof(PhysicsMass)
-        );
+    //    EntityArchetype archetype = manager.CreateArchetype
+    //    (
+    //        typeof(Translation),
+    //        typeof(Rotation),
+    //        typeof(LocalToWorld),
+    //        typeof(RenderMesh),
+    //        typeof(RenderBounds),
+    //        typeof(PhysicsVelocity),
+    //        typeof(PhysicsDamping),
+    //        typeof(PhysicsGravityFactor),
+    //        typeof(PhysicsCollider),
+    //        typeof(PhysicsMass)
+    //    );
 
-        Entity entity = manager.CreateEntity(archetype);
+    //    Entity entity = manager.CreateEntity(archetype);
 
-        manager.AddComponentData(entity, new Translation {});
-        manager.AddComponentData(entity, new Rotation {});
-        manager.AddComponentData(entity, new LocalToWorld());
-        manager.SetComponentData(entity, new RenderBounds { Value = _mesh.bounds.ToAABB() });
-        manager.AddSharedComponentData(entity, new RenderMesh
-        {
-            mesh = _mesh,
-            material = _material
-        });
+    //    manager.AddComponentData(entity, new Translation { });
+    //    manager.AddComponentData(entity, new Rotation { });
+    //    manager.AddComponentData(entity, new LocalToWorld());
+    //    manager.SetComponentData(entity, new RenderBounds { Value = _mesh.bounds.ToAABB() });
+    //    manager.AddSharedComponentData(entity, new RenderMesh
+    //    {
+    //        mesh = _mesh,
+    //        material = _material
+    //    });
 
-        // Physics
-        Vector3 initialVelocity = transform.forward * _initialSpeed;
-        manager.AddComponentData(entity, new PhysicsVelocity
-        {
-            Linear = new float3(initialVelocity),
-            Angular = new float3(0, 0, 0)
-        });
+    //    // Physics
+    //    Vector3 initialVelocity = transform.forward * _initialSpeed;
+    //    manager.AddComponentData(entity, new PhysicsVelocity
+    //    {
+    //        Linear = new float3(initialVelocity),
+    //        Angular = new float3(0, 0, 0)
+    //    });
 
-        manager.AddComponentData(entity, new PhysicsDamping()
-        {
-            Linear = 0.00f,
-            Angular = 0.005f
-        });
+    //    manager.AddComponentData(entity, new PhysicsDamping()
+    //    {
+    //        Linear = 0.00f,
+    //        Angular = 0.005f
+    //    });
 
-        manager.AddComponentData(entity, new PhysicsGravityFactor { Value = 0 });
+    //    manager.AddComponentData(entity, new PhysicsGravityFactor { Value = 0 });
 
-        PhysicsCollider collider = new PhysicsCollider
-        {
-            Value = Unity.Physics.BoxCollider.Create(new BoxGeometry
-            {
-                Center = new float3(0f, 0.5f, 0f),
-                Orientation = quaternion.identity,
-                Size = new float3(1f, 1f, 1f)
-            })
-        };
+    //    PhysicsCollider collider = new PhysicsCollider
+    //    {
+    //        Value = Unity.Physics.BoxCollider.Create(new BoxGeometry
+    //        {
+    //            Center = new float3(0f, 0.5f, 0f),
+    //            Orientation = quaternion.identity,
+    //            Size = new float3(1f, 1f, 1f)
+    //        })
+    //    };
 
-        manager.AddComponentData(entity, collider);
-        manager.AddComponentData(entity, PhysicsMass.CreateDynamic(collider.MassProperties, 1f));
+    //    manager.AddComponentData(entity, collider);
+    //    manager.AddComponentData(entity, PhysicsMass.CreateDynamic(collider.MassProperties, 1f));
 
-        return entity;
-    }
+    //    return entity;
+    //}
 
 }
